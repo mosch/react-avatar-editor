8cb867b2a566d9703b6c8dc2133e014cfc18c6dd
Daniel Lehr <daniel.lehr@allthings.me>
2017-04-13 23:10:42 +0200

    Remove babel specific options from webpack config. These are specified in .babelrc.

--- webpack.config.js
+++ webpack.config.js
@@ -7,24 +7,11 @@ module.exports = {
     inline: true 
   },
   module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: [
-            'react',
-            'es2015'
-          ],
-          plugins: [
-            'transform-class-properties',
-            'transform-object-rest-spread',
-            'transform-function-bind'
-          ]
-        }
-      }
-    ]
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
   },
   devtool: 'source-map'
-};
+}
