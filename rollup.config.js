e31fe44745418cc10bf22867885d1aacc2715ba7
Moritz Schwörer <mr.mosch@gmail.com>
2018-03-09 11:53:25 +0100

    drop uglyify

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,13 @@
-import babelrc from 'babelrc-rollup'
-import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
-import _isEqual from 'lodash/isEqual'
+import babelrc from "babelrc-rollup"
+import babel from "rollup-plugin-babel"
+import _isEqual from "lodash/isEqual"
 
-const pkg = require('./package.json')
+const pkg = require("./package.json")
 const external = Object.keys(pkg.dependencies)
 
 const config = babelrc({ addExternalHelpersPlugin: false })
 
-const whiteList = ['es2015']
+const whiteList = ["es2015"]
 
 config.presets = config.presets.map(([name, config]) => {
   if (!whiteList.includes(name) && _isEqual(config, { modules: false })) {
@@ -18,25 +17,20 @@ config.presets = config.presets.map(([name, config]) => {
   }
 })
 
-let plugins = [
-  babel(config),
-  uglify()
-]
-
 export default {
-  entry: 'src/index.js',
-  plugins,
+  entry: "src/index.js",
+  plugins: babel(config),
   external: external,
   globals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'prop-types': 'PropTypes'
+    react: "React",
+    "react-dom": "ReactDOM",
+    "prop-types": "PropTypes"
   },
   targets: [
     {
       dest: pkg.main,
-      format: 'umd',
-      moduleName: 'AvatarEditor',
+      format: "umd",
+      moduleName: "AvatarEditor",
       sourceMap: true
     }
   ]
