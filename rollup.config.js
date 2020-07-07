c870869418a3eb975444a10d90fcbe86f2f0b763
Justin McMurdie <mcmurdiej@gmail.com>
2020-07-07 09:37:28 -0600

    Upgrading babel and rollup to latest

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,30 @@
-import babel from 'rollup-plugin-babel'
-import uglify from 'rollup-plugin-uglify'
+import babel from '@rollup/plugin-babel'
+import { uglify } from 'rollup-plugin-uglify'
 
 const pkg = require('./package.json')
 const external = Object.keys(pkg.dependencies)
 
 export default {
-  entry: 'src/index.js',
+  input: 'src/index.js',
   plugins: [
     babel({
       exclude: 'node_modules/**',
-      plugins: ['external-helpers'],
+      babelHelpers: 'external',
+      plugins: ['@babel/plugin-external-helpers'],
     }),
     uglify(),
   ],
   external: external,
-  globals: {
-    react: 'React',
-    'react-dom': 'ReactDOM',
-    'prop-types': 'PropTypes',
-    'classnames': 'classNames',
-  },
-  targets: [
-    {
-      dest: pkg.main,
-      format: 'umd',
-      moduleName: 'AvatarEditor',
-      sourceMap: false,
+  output: {
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      'prop-types': 'PropTypes',
+      'classnames': 'classNames',
     },
-  ],
+    file: pkg.main,
+    format: 'umd',
+    name: 'AvatarEditor',
+    sourcemap: false,
+  },
 }
