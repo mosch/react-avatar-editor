964b922cb07065bab7e123e4ca8a2f8cff8c31b3
Daniel Lehr <daniellehr@gmx.de>
2022-05-03 23:24:58 +0200

    typescript and infra update

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 
-const root = createRoot(document.getElementById('app'));
-root.render(<App />);
+const root = createRoot(document.getElementById('app')!)
+root.render(<App />)
