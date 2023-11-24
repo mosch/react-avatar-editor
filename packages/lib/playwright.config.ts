8b7f04202ff39236a8b068faafd0860ade262607
Daniel Lehr <daniellehr@gmx.de>
2023-11-24 11:22:46 +0100

    Use dev server for test

--- packages/lib/playwright.config.ts
+++ packages/lib/playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     trace: 'on-first-retry',
   },
   webServer: {
-    command: `pnpm --filter react-avatar-editor-demo preview --host --port ${port}`,
+    command: `pnpm --filter react-avatar-editor-demo dev --host --port ${port}`,
     url: `http://127.0.0.1:${port}/index.html`,
   },
 })
