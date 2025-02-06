4609eed520a38cc4c6544c4a5a8fc69cdec12948
dongfang <1136005348@qq.com>
2025-02-06 09:28:16 +0000

    fix: remove redundant lines

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -79,14 +79,12 @@ const drawGrid = (
   context.fillRect(thirdsX + x, y, 1, height)
   context.fillRect(thirdsX * 2 + x, y, 1, height)
   context.fillRect(thirdsX * 3 + x, y, 1, height)
-  context.fillRect(thirdsX * 4 + x, y, 1, height)
 
   // horizontal bars
   context.fillRect(x, y, width, 1)
   context.fillRect(x, thirdsY + y, width, 1)
   context.fillRect(x, thirdsY * 2 + y, width, 1)
   context.fillRect(x, thirdsY * 3 + y, width, 1)
-  context.fillRect(x, thirdsY * 4 + y, width, 1)
 }
 
 const defaultEmptyImage = {
